9f2e2566bf63679dddebb51d5a328ed19c219fe6
Benoit Hubert <benoithubert@gmail.com>
2020-12-07 11:33:21 +0100

    MovieList component to repeat Movie

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
   <div id="app">
-    <Movie v-bind:movie="movie" />
+    <!-- <Movie v-bind:movie="movie" /> -->
+    <movie-list v-bind:movies="movies" />
   </div>
 </template>
 
 <script>
-import Movie from "./components/Movie";
+import MovieList from "./components/MovieList";
 
 export default {
   name: "App",
   components: {
-    Movie,
+    MovieList,
   },
   data() {
     return {
-      movie: {
-        id: 1,
-        title: "The Invisible Man",
-        picture:
-          "https://see-mag.fr/wp-content/uploads/2020/06/THE_INVISIBLE_MAN_POSTER_SEE.jpg",
-        release: "2020-02-28"
-      },
+      movies: [
+        {
+          id: 1,
+          title: "The Invisible Man",
+          picture:
+            "https://see-mag.fr/wp-content/uploads/2020/06/THE_INVISIBLE_MAN_POSTER_SEE.jpg",
+          release: "2020-02-28",
+        },
+        {
+          id: 2,
+          title: "Inception",
+          picture:
+            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
+          release: "2010-07-08",
+        },
+      ],
     };
   },
 };
---
+++ src/components/MovieList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <movie v-bind:movie="movie" v-bind:key="movie.id" v-for="movie in movies" />
+  </div>
+</template>
+
+<script>
+import Movie from './Movie'
+
+export default {
+  name: 'MovieList',
+  components: {
+    Movie
+  },
+  props: {
+    movies: Array
+  }
+}
+</script>
