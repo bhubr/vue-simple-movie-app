419803406959f952d1d79b8d953a2fa786e6fccd
Benoit Hubert <benoithubert@gmail.com>
2020-12-07 10:50:57 +0100

    Movie component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Movie />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Movie from './components/Movie'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Movie
   }
 }
 </script>
---
+++ src/components/Movie.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Movie">
+    <img src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg" alt="" />
+    <h3>Inception</h3>
+    <span>&#9733;</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Movie'
+}
+</script>
+
+<style scoped>
+.Movie {
+  position: relative;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  margin-bottom: 1em;
+}
+
+img {
+  max-width: 80px;
+  margin-right: 2em;
+}
+
+h3 {
+  flex-grow: 1;
+}
+</style>
